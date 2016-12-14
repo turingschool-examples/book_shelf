7adc20136d1a6e47bb91e40b7397be6b9cf73b21
Sal Espinosa <sespinos@gmail.com>
2016-12-14 09:37:15 -0700

    Complete form lesson and remaining CRUD

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,46 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find( params[:id] )
+    @book = Book.find(params[:id])
   end
 
   def new
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      @errors = @book.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      @errors = @book.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.delete
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :price, :quantity)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root to: "books#index"
 
-  resources :books, only: [:index, :show, :new]
-
+  resources :books
   resources :categories, only: [:index]
-
-  # get "/random_book", to: "books#random"
-
 end
